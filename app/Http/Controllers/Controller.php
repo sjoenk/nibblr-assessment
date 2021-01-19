257dc0d750016ea622c2bf112476efa9535c68a3
Paul Schunck <pschunck@icloud.com>
2021-01-19 13:19:25 +0100

    Removed use of user management helper

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,10 +6,9 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-use App\Helpers\UserManagement;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests, UserManagement;
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
 }
